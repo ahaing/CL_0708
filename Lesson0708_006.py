25597d7af965a37a31924f215337f3e7763e7fe5
ahaing <ahahaing@gmail.com>
2023-07-08 14:42:50 +0800

    fsdsa

--- Lesson0708_006.py
+++ Lesson0708_006.py
@@ -8,12 +8,13 @@ target = random.randint(min, max)
 print(f"猜數字遊戲{min}~{max}\n\n")
 
 while True:
-    keyin = int(input(f"猜數字遊戲{min}~{max}"))
+    keyin = int(input(f"猜數字遊戲{min}~{max}:"))
     count += 1
     if (keyin == target):
-        print(f" ~ BINGO! Congrats!!! The number is {target}~ ")
-        print(f"猜了共{count}次")
+        print(f" ~ Congrats!!! The number is {target} ~ BINGO!! ~ ")
+        print(f"== 猜了共{count}次 ==")
+        break
     else:
-        print("Sorry You Lose")
-        print(f"猜錯第{count}次")
-print("Game Over")
+        print("Sorry You're wrong")
+        print(f"猜錯第{count}次\n")
+print("\nGame Over\n")
